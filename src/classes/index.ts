9732514c6f9b87dab8927f8094a586b0a4ca597c
Prince Patel <ext_prpatel@falabella.cl>
2023-01-05 11:11:12 +0530

    update configs

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,2 +1,1 @@
 export { addPrefix, joinClass, mapClass } from './classes'
-export type { AddPrefixType, JoinClassType, MapClassType } from './types'
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import EsLint from 'vite-plugin-linter'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'node:path'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
+const { EsLinter, linterPlugin } = EsLint
+
+export default defineConfig((configEnv) => ({
+  plugins: [
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx,js,jsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['src/**/*'],
+    }),
+  ],
   build: {
     lib: {
-      entry: "./src/index.ts",
-      name: "Kreattix Utils",
-      fileName: "index",
-      formats: ["es", "cjs"],
+      entry: resolve('src', 'index.ts'),
+      name: 'Kreattix Utils',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index${format === 'cjs' ? '' : '.' + format}.js`,
     },
+    sourcemap: true,
+    minify: 'esbuild',
   },
-  plugins: [dts()],
-});
+}))
