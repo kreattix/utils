56911ec3c849f34315a2e2e14752eb7a4cba0196
Prince Patel <prince.patel@ey.com>
2024-01-18 13:20:28 +0530

    add test cases

--- src/classes/__test__/classes.test.ts
+++ src/classes/__test__/classes.test.ts
@@ -1,4 +1,4 @@
-import { addPrefix, joinClass, mapClass } from '../classes'
+import { addPrefix, classnames, joinClass, mapClass, prefixClassnames } from '../classes'
 
 describe('check class functions', () => {
   describe('check joinClass function', () => {
@@ -39,5 +39,59 @@ describe('check class functions', () => {
       expect(mapClass(null, 'class1')).toBe('class1')
       expect(mapClass(null, 'class1', 'staicClass')).toBe('class1 staicClass')
     })
+  })
+})
+
+describe('prefixClassnames', () => {
+  test('should prefix a single string argument', () => {
+    expect(prefixClassnames('prefix', 'classname')).toBe('prefix-classname')
+  })
+
+  test('should not concatenate booleon arguments', () => {
+    expect(prefixClassnames('prefix', true, false)).toBe('')
+  })
+
+  test('should prefix multiple string arguments', () => {
+    expect(prefixClassnames('prefix', 'class1', 'class2')).toBe('prefix-class1 prefix-class2')
+  })
+
+  test('should ignore falsy arguments', () => {
+    expect(prefixClassnames('prefix', 'class1', null, 'class2', undefined)).toBe(
+      'prefix-class1 prefix-class2',
+    )
+  })
+
+  test('should recursively prefix nested arrays', () => {
+    expect(prefixClassnames('prefix', ['class1', ['class2', 'class3']])).toBe(
+      'prefix-class1 prefix-class2 prefix-class3',
+    )
+  })
+
+  test('should prefix object keys with truthy values', () => {
+    expect(prefixClassnames('prefix', { class1: true, class2: false, class3: true })).toBe(
+      'prefix-class1 prefix-class3',
+    )
+  })
+})
+
+describe('classnames', () => {
+  test('should concatenate string arguments', () => {
+    expect(classnames('class1', 'class2')).toBe('class1 class2')
+  })
+
+  test('should not concatenate booleon arguments', () => {
+    expect(classnames(true, false)).toBe('')
+  })
+
+  test('should ignore falsy arguments', () => {
+    expect(classnames('class1', null, 'class2', undefined)).toBe('class1 class2')
+  })
+
+  test('should recursively concatenate nested arrays', () => {
+    expect(classnames(['class1', ['class2', 'class3']])).toBe('class1 class2 class3')
+  })
+
+  test('should concatenate object keys with truthy values', () => {
+    expect(classnames({ class1: true, class2: false, class3: true })).toBe('class1 class3')
   })
 })
